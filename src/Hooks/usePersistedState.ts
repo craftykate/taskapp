22886a452779483e4b5fefcbe0c51ff9d9723222
Kate <katemcfaul@gmail.com>
2021-12-29 10:47:54 -0800

    Add idb-keyval

---
+++ src/Hooks/usePersistedState.ts
@@ -0,0 +1,26 @@
+import React from 'react'
+import { set, get } from 'idb-keyval'
+
+export function usePersistedState<TState>(
+  keyToPersistWith: string,
+  defaultState: TState
+) {
+  const [state, setState] = React.useState<TState | undefined>(undefined)
+
+  React.useEffect(() => {
+    get<TState>(keyToPersistWith).then((retrievedState) =>
+      // If a value is retrieved then use it; otherwise default to defaultValue
+      setState(retrievedState ?? defaultState)
+    )
+  }, [keyToPersistWith, setState, defaultState])
+
+  const setPersistedValue = React.useCallback(
+    (newValue: TState) => {
+      setState(newValue)
+      set(keyToPersistWith, newValue)
+    },
+    [keyToPersistWith, setState]
+  )
+
+  return [state, setPersistedValue] as const
+}
